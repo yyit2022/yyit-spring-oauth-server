848024da170f669017a48cd563ab3d8f7e4f7124
yanpingping <yanpp@nfky.com>
2022-05-10 11:02:43 +0800

    test

--- src/main/java/com/yyit/oauth/server/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/yyit/oauth/server/configuration/SpringSecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SpringSecurityConfiguration {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authorizeRequests ->
-                        authorizeRequests.antMatchers("/oauth2/**").permitAll().anyRequest().authenticated()
+                        authorizeRequests.anyRequest().authenticated()
                 )
                 .httpBasic()
                 .and()
